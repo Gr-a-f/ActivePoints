4d035bb99cf0c56fc8aa660facef2b28341b6caa
Артем <@DESKTOP-IHDL6B9>
2024-12-21 13:46:22 +0300

    Add project files.

---
+++ ActivePoints/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using ScottPlot;
+
+namespace ActivePoints
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // There should be a total of 1953 points after all the transformations
+        public double[] myI = new double[1953];
+        public double[] myU = new double[1953];
+        public double[] myx = new double[1953];
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] ports = SerialPort.GetPortNames();
+            COMbox.ItemsSource = ports;
+            if (ports.Length > 0)
+            {
+                COMbox.SelectedIndex = 0;
+            }
+        }
+
+        public static void DataProcessing(List<string> Data, ref double[] myI, ref double[] myU, ref double[] myx)
+        {
+
+            //Dividing the data into current and voltage arrays
+            var BinString = Data;
+            var i0 = new List<string>();
+            var u0 = new List<string>();
+
+            try
+            {
+                for (int i = 0; i < Data.Count; i++)
+                {
+                    if (BinString[i].Substring(0, 4) == "0100")
+                    {
+                        i0.Add(BinString[i].Substring(4) + BinString[i + 1]
+                            + BinString[i + 2] + BinString[i + 3]
+                            + BinString[i + 4]);
+                    }
+                    else if (BinString[i].Substring(0, 4) == "0110")
+                    {
+                        u0.Add(BinString[i].Substring(4) + BinString[i + 1]
+                            + BinString[i + 2] + BinString[i + 3]
+                            + BinString[i + 4]);
+                    }
+                }
+
+
+                //Conversion of data into numerical format
+
+                //Errors in this section usually occur when the electrode is pressed several times
+                //during the measurement without waiting for the measurement to complete.
+
+                string ubin;
+                string ibin;
+
+                for (int i = 0; i < i0.Count; i++)
+                {
+                    ibin = i0[i][0] + i0[i].Substring(5, 7)
+                        + i0[i][1] + i0[i].Substring(13, 7)
+                        + i0[i][2] + i0[i].Substring(21, 7)
+                        + i0[i][3] + i0[i].Substring(29, 7);
+                    myI[i] = Convert.ToInt32(ibin, 2);
+                    myx[i] = i;
+                }
+
+                for (int i = 0; i < u0.Count; i++)
+                {
+                    ubin = u0[i][0] + u0[i].Substring(5, 7)
+                        + u0[i][1] + u0[i].Substring(13, 7)
+                        + u0[i][2] + u0[i].Substring(21, 7)
+                        + u0[i][3] + u0[i].Substring(29, 7);
+                    myU[i] = Convert.ToInt32(ubin, 2);
+                }
+            }
+            catch { }
+        }
+
+        private void Bstart_Click(object sender, RoutedEventArgs e)
+        {
+            if (COMbox.SelectedValue == null)
+            {
+                TMstatus.Text = "COM port has not been selected";
+            }
+            else
+            {
+                Meashure();
+            }
+        }
+        private void Meashure()
+        {
+            TMstatus.Text = "Measurement in progress";
+            var port = new MySerialPort();
+            port.Open(COMbox.SelectedValue.ToString());
+            while (!port.isend)
+            {
+            }
+
+            DataProcessing(port.DataStringBinary, ref myI, ref myU, ref myx);
+            Plot();
+
+            TMstatus.Text = "Measurement completed";
+        }
+        private void Plot()
+        {
+            ResultU.Plot.Clear();
+            ResultI.Plot.Clear();
+            ResultI.Plot.Add.Scatter(myx, myI);
+            ResultU.Plot.Add.Scatter(myx, myU);
+            ResultI.Plot.Axes.AutoScale();
+            ResultU.Plot.Axes.AutoScale();
+            ResultU.Refresh();
+            ResultI.Refresh();
+        }
+    }
+}
---
+++ ActivePoints/MySerialPort.cs
@@ -0,0 +1,59 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ActivePoints
+{
+    /// <summary>
+    /// Serial port resource with specified parameters
+    /// </summary>
+    public class MySerialPort : SerialPort
+    {
+        //
+        public List<string> DataStringBinary = new List<string>();
+        public bool isend = false;
+        public MySerialPort()
+            : base()
+        {
+            base.BaudRate = 430800;
+            base.DataBits = 8;
+            base.StopBits = StopBits.One;
+            base.Parity = Parity.None;
+
+            base.DataReceived += OnDataReceived;
+        }
+
+        public void Open(string portName)
+        {
+            if (base.IsOpen)
+            {
+                base.Close();
+            }
+            base.PortName = portName;
+            base.Open();
+        }
+        private void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var serialDevice = sender as SerialPort;
+            var buffer = new byte[serialDevice.BytesToRead];
+            serialDevice.Read(buffer, 0, buffer.Length);
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                string sbt = Convert.ToString(buffer[i], 2).PadLeft(8, '0');
+                DataStringBinary.Add(sbt);
+            }
+
+            if (buffer[^1]==7)
+            {
+                isend = true;
+                base.Close();
+            }
+        }
+    }
+}
